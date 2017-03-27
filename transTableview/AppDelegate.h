86bccbe2f91c0e485d5daff0aef95e3f84c1d47f
tianNanYiHao <851085835@qq.com>
2017-03-27 10:21:54 +0800

    Initial Commit

---
+++ transTableview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  transTableview
+//
+//  Created by tianNanYiHao on 2017/3/27.
+//  Copyright © 2017年 tianNanYiHao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
